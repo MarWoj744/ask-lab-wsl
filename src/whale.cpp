690a2a59e230613029966845c42910b523d7ceff
MarWoj744 <72458177+MarWoj744@users.noreply.github.com>
2022-06-26 12:13:13 +0200

    Add files via upload
    dzialaj plz

---
+++ src/whale.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{  
+	cout << endl <<
+		"          ##         ." << endl <<
+		"    ## ## ##        ==" << endl <<
+		" ## ## ## ## ##    ===" << endl <<
+		"/\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\___/ ===" << endl <<
+		"{                       /  ===-" << endl <<
+		"\\______ O           __/" << endl <<
+		" \\    \\         __/" << endl <<
+		"  \\____\\_______/" << endl <<
+		endl << endl <<
+		"Hello from WSL!" << endl;
+	return 0;
+}
+
+ 
